04b0c57328fedb92e7d00a0761647bafa2bb7ee8
Anıl Uğur YELDAN <37743757+doctorwho1110@users.noreply.github.com>
2020-05-07 22:59:03 +0300

    function deletion
    def divide_into_chunks deleted

--- CMP_2204/P2P_Server.py
+++ CMP_2204/P2P_Server.py
@@ -6,27 +6,6 @@ import datetime
 import math
 import socket
 
-
-def divide_into_chunks(file, fileName, directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    c = os.path.getsize(file)
-    CHUNK_SIZE = math.ceil(math.ceil(c) / 5)
-    cnt = 1
-    with open(file, 'rb') as infile:
-        divided_file = infile.read(int(CHUNK_SIZE))
-        while divided_file:
-            name = directory + "/" + fileName.split('.')[0] + "" + str(cnt)
-            with open(name, 'wb+') as div:
-                div.write(divided_file)
-            cnt += 1
-            divided_file = infile.read(int(CHUNK_SIZE))
-
-    user_file = input("Which file do you want to host ? ")
-
-    divide_into_chunks(user_file, )
-
-
 class Server:
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
