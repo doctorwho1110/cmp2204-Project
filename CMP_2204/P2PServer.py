46506eec348af430a08ccf8b534200aac3402fed
Anıl Uğur YELDAN <37743757+doctorwho1110@users.noreply.github.com>
2020-05-07 22:56:33 +0300

    Düzeltildi
    P2P_server düzeltildi

--- CMP_2204/P2PServer.py
+++ CMP_2204/P2PServer.py
@@ -6,7 +6,7 @@ import datetime
 import math
 import socket
 
-<<<<<<< HEAD
+
 def divide_into_chunks(file, fileName, directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
@@ -24,34 +24,34 @@ def divide_into_chunks(file, fileName, directory):
 
     user_file = input("Which file do you want to host ? ")
 
+    divide_into_chunks(user_file, )
 
-    divide_into_chunks(user_file,)
-=======
+
 class Server:
     def __init__(self):
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.accept_connections()
-    
+
     def accept_connections(self):
         ip = socket.gethostbyname(socket.gethostname())
         port = int(input('Enter desired port --> '))
 
-        self.s.bind((ip,port))
+        self.s.bind((ip, port))
         self.s.listen(10)
 
-        print('Running on IP: '+ip)
-        print('Running on port: '+str(port))
+        print('Running on IP: ' + ip)
+        print('Running on port: ' + str(port))
 
         while 1:
             c, addr = self.s.accept()
-            print("Got connection from",addr)
+            print("Got connection from", addr)
             print(c)
-            
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
-    def handle_client(self,c,addr):
+            threading.Thread(target=self.handle_client, args=(c, addr,)).start()
+
+    def handle_client(self, c, addr):
         while True:
-            
+
             filename = c.recv(4096)
 
             if not os.path.exists(filename):
@@ -63,18 +63,18 @@ class Server:
                     c.send("EXISTS" + str(os.path.getsize(filename)))
                     userResponse = c.recv(1024)
                     if userResponse[:2] == 'OK':
-                        with open(filename,"rb") as fn:
+                        with open(filename, "rb") as fn:
                             btyesToSend = fn.read(os.path.getsize(filename))
                             c.send(btyesToSend)
-                            logins = open("P2P.txt","a")
+                            logins = open("P2P.txt", "a")
                             timenow = datetime.datetime.now().strftime("%H:%M:%S %d-%m-%Y")
                             logins.write(timenow + "," + addr[0] + "," + filename + "\n")
                             logins.close()
                             break
         c.shutdown(socket.SHUT_RDWR)
         c.close()
-    
-    def divide_into_chunks(self,file, fileName, directory):
+
+    def divide_into_chunks(self, file, fileName, directory):
         if not os.path.exists(directory):
             os.makedirs(directory)
         c = os.path.getsize(file)
@@ -89,5 +89,5 @@ class Server:
                 cnt += 1
                 divided_file = infile.read(int(CHUNK_SIZE))
 
+
 server = Server()
->>>>>>> 3a3e31b76c383e995fe5c110bae01fd547b602d7
