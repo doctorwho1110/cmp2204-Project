8b0361d21838ded77c84c803c562cb4beaf9b271
Anıl Uğur YELDAN <37743757+doctorwho1110@users.noreply.github.com>
2020-05-08 00:11:43 +0300

    handle_client
    handle_client ve divide_into_chunks birleştirildi

--- CMP_2204/P2P_Server.py
+++ CMP_2204/P2P_Server.py
@@ -7,13 +7,14 @@ import math
 import socket
 
 class Server:
+
     def __init__(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.accept_connections()
 
     def accept_connections(self):
         ip = socket.gethostbyname(socket.gethostname())
-        port = int(input('Enter desired port --> '))
+        port = 5001
 
         self.s.bind((ip, port))
         self.s.listen(10)
@@ -28,34 +29,9 @@ class Server:
 
             threading.Thread(target=self.handle_client, args=(c, addr,)).start()
 
-    def handle_client(self, c, addr):
-        while True:
-
-            filename = c.recv(4096)
-
-            if not os.path.exists(filename):
-                c.send("File does not exist".encode())
-            else:
-                filename = "p2pfiles/" + json.loads(filename.decode())["files"]
-                print("Files received ...")
-                if os.path.isfile(filename):
-                    c.send("EXISTS" + str(os.path.getsize(filename)))
-                    userResponse = c.recv(1024)
-                    if userResponse[:2] == 'OK':
-                        with open(filename, "rb") as fn:
-                            btyesToSend = fn.read(os.path.getsize(filename))
-                            c.send(btyesToSend)
-                            logins = open("P2P.txt", "a")
-                            timenow = datetime.datetime.now().strftime("%H:%M:%S %d-%m-%Y")
-                            logins.write(timenow + "," + addr[0] + "," + filename + "\n")
-                            logins.close()
-                            break
-        c.shutdown(socket.SHUT_RDWR)
-        c.close()
-
-    def divide_into_chunks(self, file, fileName, directory):
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+    def  divide_into_chunks(self, file, fileName, directory):
+        '''if not os.path.exists(directory):
+            os.makedirs(directory)'''
         c = os.path.getsize(file)
         CHUNK_SIZE = math.ceil(math.ceil(c) / 5)
         cnt = 1
@@ -67,6 +43,37 @@ class Server:
                     div.write(divided_file)
                 cnt += 1
                 divided_file = infile.read(int(CHUNK_SIZE))
+
+    def handle_client(self, c, addr,fileName,directory):
+
+        while True:
+            filename = c.recv(4096)
+            if not os.path.exists(filename): #Filename dosyası osde yoksa
+                c.send("File does not exist".encode())  # Mesajı gönder
+                os.makedirs(directory)
+            else:
+                filename = "p2pfiles/" + json.loads(filename.decode())["files"] # İsim ataması
+                print("Files received ...")
+                c.send("EXISTS" + str(os.path.getsize(filename))) #Dosyanı  bulunduğuna dair kullanıcıya mesaj yolla
+                userResponse = c.recv(1024) # Kullanıcının mesajını al
+                if userResponse[:2] == 'OK':
+                    with open(filename, "rb") as fn: #filename adında bir dosya aç
+                        bytesToSend = fn.read(os.path.getsize(filename))
+                        CHUNK_SIZE=math.ceil(math.ceil(bytesToSend) / 5)
+                        #c.send(bytesToSend)
+                        cnt = 1
+                        divided_file = fn.read(int(CHUNK_SIZE))
+                        while divided_file:
+                            name = directory + "/" + fileName.split('.')[0] + "" + str(cnt)  # Hatalı olabilir
+                            with open(name, 'wb+') as div:
+                                div.write(divided_file)
+                            cnt += 1
+                            divided_file = fn.read(int(CHUNK_SIZE))
+            break
+        c.shutdown(socket.SHUT_RDWR)
+        c.close()
+
+
 
 
 server = Server()
