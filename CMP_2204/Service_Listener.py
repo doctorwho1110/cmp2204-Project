2b2953f253b2bcb0100de641486ee37a9def5e0d
Anıl Uğur YELDAN <37743757+doctorwho1110@users.noreply.github.com>
2020-05-10 00:57:34 +0300

    Service_Listener
    Ali Service_Listener update

--- CMP_2204/Service_Listener.py
+++ CMP_2204/Service_Listener.py
@@ -1,21 +1,41 @@
 import socket
 import json
 import sys
+import os
+
 
 files = {}
-bufferSize = 2048
+bufferSize = 4096
 
 
 def listenerIp():
     ListenerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    MyIp = socket.gethostbyname(socket.gethostname()).split(".")
-    MyIp = "{}.{}.{}.255'".format(MyIp[0], MyIp[1], MyIp[2])
+    ListenerSocket.connect(("1.1.1.1", 1))
+    ip = ListenerSocket.getsockname()[0].split(".")
+    MyIp = ip[0] + "." + ip[1] + "." + ip[2] + ".255"
+
     ListenerSocket.close()
     return MyIp
 
 
+def create_userfile(info, ip):
+    if ip != listenerIp():
+        data = json.loads(info.decode("utf-8"))
+        if data["username"] is None:
+            pass
+        for file in data["files"]:
+            files[file] = ip
+        for key in files:
+            print("Username" + data["username"])
+            print("File: " + key)
+            print("IP : " + files[key])
+        with open("store.txt", "w") as f:
+            f.write(json.dumps(files))
+
+
 def listening(adres):
     print("Service Listening...")
+
     try:
         UDPListenerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         UDPListenerSocket.bind(adres)
@@ -23,27 +43,21 @@ def listening(adres):
         print("Caught exception socket.error : %s" % e)
     while True:
         info, address = UDPListenerSocket.recvfrom(bufferSize)
-        ip = address[0], portL = address[1]
-        # msg = b"Listener listening..."
-        # UDPListenerSocket.sendto(msg,(ip,port))
 
-        if ip != listenerIp():
-            data = json.loads(str(info.decode()))
-            if data["username"] is None:
-                continue
-            for file in data["files"]:
-                files[file] = ip
-            for key in files:
-                print("Username" + data["username"])
-                print("File: " + key)
-                print("IP : " + files[key])
-            with open("store.txt", "w") as f:
-                f.write(json.dumps(files))
+        ip = address[0]
+        create_userfile(info, ip)
 
 
 def main():
     ip = listenerIp()
-    listening((ip, 5000))
+
+    print(ip)
+    if os.name == 'nt':
+        print("Running On Windows")
+        listening((listenerIp(), 5000))
+    else:
+        print("Running On Unix-Like Systems")
+        listening((ip, 5000))
 
 
 if __name__ == "__main__":
